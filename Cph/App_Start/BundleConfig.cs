0f1be644e6fd7838c36d05c62d3f89e64c95263d
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2013-10-11 15:37:49 +0300

    Created empty MVC 4 site

---
+++ Cph/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Cph
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+            // jquery
+            bundles.Add(new ScriptBundle("~/bundles/jquery",
+                cdnPath: "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js").Include(
+                "~/js/jquery-{version}.js"
+            ));
+
+            // js
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/js/jquery.unobtrusive*",
+                "~/js/jquery.validate*"
+            ));
+
+            // css
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/css/site.css"
+            ));
+        }
+    }
+}
---
+++ Cph/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cph.MvcApplication" Language="C#" %>
