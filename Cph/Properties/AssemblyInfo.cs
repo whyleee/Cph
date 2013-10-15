965620bc31e6d2dc9f937bdce7e11df51fb791b1
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2013-10-15 13:18:15 +0300

    Set v0.1 and added link to github in footer

--- Cph/Properties/AssemblyInfo.cs
+++ Cph/Properties/AssemblyInfo.cs
@@ -1,35 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General info about the product
 [assembly: AssemblyTitle("Cph")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("CPH Team internal website")]
+[assembly: AssemblyCompany("Creuna CPH")]
 [assembly: AssemblyProduct("Cph")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © Creuna CPH 2013")]
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3929facf-4279-42cc-a37b-0c23920c24d1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Product version
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyInformationalVersion("First public release")]
